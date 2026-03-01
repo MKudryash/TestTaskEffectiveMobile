380253e54c38011b08b56b83bd0fd80068d420d1
MKudryash <jukudryash@gmail.com>
2026-03-01 13:08:53 +0300

    creating a module structure

---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TestTaskEffectiveMobile"
+include(":app")
+include(":core")
+include(":feature")
+include(":core:design")
+include(":core:network")
+include(":core:database")
+include(":feature:auth")
+include(":feature:main")
+include(":feature:favorite")
+include(":feature:account")
+include(":navigation")
